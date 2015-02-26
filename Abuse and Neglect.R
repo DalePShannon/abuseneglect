fa105640c1f1db2fdc281c4e0977326886cf30a3
dalepshannon@gmail.com <dalepshannon@gmail.com>
2015-02-26 09:08:21 -0500

    code not saved on previous commit
    Subtowns dropped.  code to check number rows added

--- Abuse and Neglect.R
+++ Abuse and Neglect.R
@@ -30,7 +30,7 @@ names(abuse.final)[c(4,5,6,7)] <-
     "Allegation Type",
     "Report Status",
     "Measure Type")
-
+abuse.final.not <- abuse.final[abuse.final$FIPS=="0NA",]
 abuse.final <- abuse.final[!abuse.final$FIPS=="0NA",]
 
 write.csv(abuse.final,"abuse neglect mod.csv",row.names=FALSE, quote=FALSE)
